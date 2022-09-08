004d1f5279950409afb4c377d942d056b437195e
AusintJagua <46783484+austin0072009@users.noreply.github.com>
2022-09-08 12:41:18 +0800

    feat(front): signpad adjust

--- src/App.js
+++ src/App.js
@@ -5,14 +5,40 @@ import Check from "./pages/Check";
 import { Routes, Route, Link } from "react-router-dom";
 import { FloatingBubble } from 'antd-mobile';
 import { MessageFill } from 'antd-mobile-icons';
+import { useEffect,useState } from "react";
+import axios from "axios";
 // import "./App.css";
 
 function App() {
 
+  let [kefuUrl,setKefu] = useState();
 
   const onClick = () => {
     // window.location.href= "http://154.38.116.197:788/index/index/user";
   }
+
+  useEffect(()=>{
+
+    async function getKefuUrl(){
+
+ let checkUrl = "/user/index/kefu";
+    let result = await axios.post(checkUrl).then(res => {
+
+      console.log(res.data.message);
+      return res.data.message.kefu_url;
+    })
+
+    setKefu(result);
+
+    }
+
+    getKefuUrl();
+   
+
+
+
+
+  },[])
 
   return (
     <div className="root">
@@ -21,7 +47,7 @@ function App() {
         <Route path="sign" element={<Sign />} />
         <Route path="check" element={<Check/>} />
       </Routes>
-      <a href="http://154.38.116.197:788/index/index/user">
+      <a href={kefuUrl}>
         <FloatingBubble
           axis='x'
           magnetic='x'
---
+++ src/pages/SignPage.js
@@ -0,0 +1,19 @@
+import { NavBar, Toast, Space, Button, Image, Modal } from "antd-mobile";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import "./Home.css"
+import sign from "../imgs/Sign.png";
+import { HandSigned } from "@react-goodies/hand-signed";
+import axios from "axios";
+
+
+function SignPage() {
+  return(
+<div>
+    
+</div>
+  )
+
+  }
+export default SignPage;
+
